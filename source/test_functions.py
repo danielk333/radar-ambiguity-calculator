ff6aa4d78189d378796e6a304fa15e35423a1de6
Felipe Betancourt <andbet-7@student.ltu.se>
2018-11-27 09:19:26 +0100

    Created testing file using unittest. Simplified script enclosing it into functions.

---
+++ source/test_functions.py
@@ -0,0 +1,100 @@
+import unittest
+from functions import *
+import numpy as np
+
+
+class TestFunctions(unittest.TestCase):
+
+    def test_lambda0(self):
+
+        self.assertEqual(lambda0(frequency = 31), 9.670724451612903)
+
+    def test_intersections_cal(self):
+
+        pinv_norm = np.array([0.06, 0.01, 0.03, 0.15, 0.04])
+        intersection_line_norm = np.array([0.75, 0, 3, 1, 0.75])
+        PERMS_J = [([0, 0], 1), ([1, 1], 1), ([2, 2], 2), ([3, 3], 3), ([4, 4], 4)]
+        R = [[0, 0, -2, 2.5], [2, -2.5, 0, 0], [0, 0, 0, 0]]
+        intersection_line = np.array([[2, 1.5, 1, 0.5, 0], [-2.22e-16, -2.22e-16, -2.22e-16, -2.22e-16, -2.22e-16], \
+                                      [0, 0, 0, 0, 0]])
+
+        intersections = intersections_cal(pinv_norm=pinv_norm, mp_tol=0.1, PERMS_J=PERMS_J, \
+                                          intersection_line=intersection_line, R=R, norm=intersection_line_norm)
+
+        try:
+            np.testing.assert_array_equal(intersections['indexes'], [[0, 4]])
+            res = True
+        except AssertionError as err:
+            res = False
+            print(err)
+
+        self.assertTrue(res)
+
+        try:
+            np.testing.assert_array_equal(intersections['permutations'], [PERMS_J[0], PERMS_J[4]])
+            res1 = True
+        except AssertionError as err1:
+            res1 = False
+            print(err1)
+
+        self.assertTrue(res1)
+
+        self.assertEqual(intersections['number'], 2)
+        self.assertEqual(np.shape(intersections['integers'])[1], 2)
+
+    def test_permutations_create(self):
+
+        PERMS_J_base = [[1, 1, 1], [1, 1, 2], [1, 1, 3], [1, 1, 4]]
+
+        indexes = [[1, 3], [0, 0]]
+
+        k_length = [2, 3]
+
+        ii = 1
+
+        PERMS_J = permutations_create(permutations_base=PERMS_J_base, intersections_ind=indexes, k_length=k_length, \
+                                      permutation_index=ii)
+
+        self.assertEqual(len(PERMS_J), len(np.array(PERMS_J_base)[indexes[0], :]) * k_length[1])
+
+    def test_rRrho_cal(self):
+
+        Sn = 4
+        Zn = 1
+        xycoords = np.array([[0, 2], [0, -2.5], [-2, 0], [2.5, 0], [0, 0]])
+        xpos = np.zeros((np.shape(xycoords)[0], 1))
+        ypos = np.zeros((np.shape(xycoords)[0], 1))
+        zpos = np.zeros((np.shape(xycoords)[0], 1))
+
+        r, R, rho = rRrho_cal(sensor_groups=Sn, subgroup_size=Zn, xycoords=xycoords, xpos=xpos, ypos=ypos, zpos=zpos)
+
+        try:
+            np.testing.assert_array_equal(np.array(np.shape(r)), np.array([3, Zn, Sn]))
+            res0 = True
+        except AssertionError as err0:
+            res0 = False
+            print(err0)
+
+        self.assertTrue(res0)
+
+        try:
+            np.testing.assert_array_equal(np.array(np.shape(R)), np.array([3, Sn]))
+            res1 = True
+        except AssertionError as err1:
+            res1 = False
+            print(err1)
+
+        self.assertTrue(res1)
+
+        try:
+            np.testing.assert_array_equal(np.array(np.shape(rho)), np.array(np.shape(r)))
+            res2 = True
+        except AssertionError as err2:
+            res2 = False
+            print(err2)
+
+        self.assertTrue(res2)
+
+
+if __name__ == '__main__':
+    unittest.main()
