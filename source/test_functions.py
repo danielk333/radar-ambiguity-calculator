29d77d7e386a5de4cb1f7042606b17af190b9c97
Felipe Betancourt <andbet-7@student.ltu.se>
2018-12-07 16:42:05 +0100

    Created plots_generator

--- source/test_functions.py
+++ source/test_functions.py
@@ -7,7 +7,7 @@ class TestFunctions(unittest.TestCase):
 
     def test_lambda0(self):
 
-        self.assertEqual(lambda0(frequency = 31), 9.670724451612903)
+        self.assertEqual(lambda0(frequency=31), 9.670724451612903)
 
     def test_intersections_cal(self):
 
@@ -15,10 +15,11 @@ class TestFunctions(unittest.TestCase):
         intersection_line_norm = np.array([0.75, 0, 3, 1, 0.75])
         PERMS_J = [([0, 0], 1), ([1, 1], 1), ([2, 2], 2), ([3, 3], 3), ([4, 4], 4)]
         R = [[0, 0, -2, 2.5], [2, -2.5, 0, 0], [0, 0, 0, 0]]
-        intersection_line = np.array([[2, 1.5, 1, 0.5, 0], [-2.22e-16, -2.22e-16, -2.22e-16, -2.22e-16, -2.22e-16], \
+        intersection_line = np.array([[2, 1.5, 1, 0.5, 0],
+                                      [-2.22e-16, -2.22e-16, -2.22e-16, -2.22e-16, -2.22e-16],
                                       [0, 0, 0, 0, 0]])
 
-        intersections = intersections_cal(pinv_norm=pinv_norm, mp_tol=0.1, PERMS_J=PERMS_J, \
+        intersections = intersections_cal(pinv_norm=pinv_norm, mp_tol=0.1, PERMS_J=PERMS_J,
                                           intersection_line=intersection_line, R=R, norm=intersection_line_norm)
 
         np.testing.assert_array_equal(intersections['indexes'], [[0, 4]])
@@ -33,25 +34,20 @@ class TestFunctions(unittest.TestCase):
         k_length = [2, 3]
         ii = 1
 
-        PERMS_J = permutations_create(permutations_base=PERMS_J_base, intersections_ind=indexes, k_length=k_length, \
+        PERMS_J = permutations_create(permutations_base=PERMS_J_base, intersections_ind=indexes, k_length=k_length, 
                                       permutation_index=ii)
 
         self.assertEqual(len(PERMS_J), len(np.array(PERMS_J_base)[indexes[0], :]) * k_length[1])
 
-    def test_rRrho_cal(self):
+    def test_R_cal(self):
 
         Sn = 4
-        Zn = 1
+
         xycoords = np.array([[0, 2], [0, -2.5], [-2, 0], [2.5, 0], [0, 0]])
-        xpos = np.zeros((np.shape(xycoords)[0], 1))
-        ypos = np.zeros((np.shape(xycoords)[0], 1))
-        zpos = np.zeros((np.shape(xycoords)[0], 1))
 
-        r, R, rho = rRrho_cal(sensor_groups=Sn, subgroup_size=Zn, xycoords=xycoords, xpos=xpos, ypos=ypos, zpos=zpos)
+        R = R_cal(sensor_groups=Sn,  xycoords=xycoords)
 
-        np.testing.assert_array_equal(np.array(np.shape(r)), np.array([3, Zn, Sn]))
         np.testing.assert_array_equal(np.array(np.shape(R)), np.array([3, Sn]))
-        np.testing.assert_array_equal(np.array(np.shape(rho)), np.array(np.shape(r)))
 
     def test_linCoeff_cal(self):
 
@@ -98,16 +94,21 @@ class TestFunctions(unittest.TestCase):
         xy = np.array([[-0.25, 0.25], [0.25, -0.25]])
         k0 = [.8, .15]
 
-        distances, normal = explicit(intersection_line, intersections_ind, cap_intersections_of_slines, xy, k0)
+        distances, normal, k_finds = explicit(intersection_line, intersections_ind, cap_intersections_of_slines, xy, k0)
 
         np.testing.assert_array_almost_equal(distances, np.array([0.6602832, 1.66250775]), decimal=8)
-        np.testing.assert_array_almost_equal(normal, np.array([[0.67249851-0.21850801j, -0.27059805+0.65328148j], [0.67249851+0.21850801j, -0.27059805-0.65328148j]]), decimal=8)
+        np.testing.assert_array_almost_equal(normal, np.array([[0.67249851-0.21850801j, -0.27059805+0.65328148j],
+                                                               [0.67249851+0.21850801j, -0.27059805-0.65328148j]]),
+                                             decimal=8)
 
     def test_slines_intersections(self):
 
         k0 = [0.15, 0.20]
         intersections_ind = [0, 2, 4]
-        intersection_line = np.array([[.1, .2, .3, .4, .5], [.6, .7, .8, .9, .8], [.7, .6, .5, .4, .3], [.2, .1, .2, .3, .4]])
+        intersection_line = np.array([[.1, .2, .3, .4, .5],
+                                      [.6, .7, .8, .9, .8],
+                                      [.7, .6, .5, .4, .3],
+                                      [.2, .1, .2, .3, .4]])
         cutoff_ph_ang = np.radians(40)
 
         cap = slines_intersections(k0, intersections_ind, intersection_line, cutoff_ph_ang)
